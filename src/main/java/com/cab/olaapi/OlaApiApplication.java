e2964f118c9469275fca3da70308c025408575d5
bipro <biprocuet@gmail.com>
2024-03-29 01:37:41 +0600

    project initialization

---
+++ src/main/java/com/cab/olaapi/OlaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cab.olaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaApiApplication.class, args);
+	}
+
+}
