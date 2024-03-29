dc5314dfe19c2c4376c1dbf8f541c1e9d6ef7b7f
bipro <biprocuet@gmail.com>
2024-03-29 16:10:03 +0600

    security config complete

--- src/main/java/com/cab/olaapi/config/SecurityConfig.java
+++ src/main/java/com/cab/olaapi/config/SecurityConfig.java
@@ -1,5 +1,96 @@
 package com.cab.olaapi.config;
 
+import com.cab.olaapi.config.jwtAuth.JwtTokenUtils;
+import com.cab.olaapi.config.userConfig.UserDetailsConfig;
+import com.cab.olaapi.repository.RefreshTokenRepository;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsConfig userDetailsConfig;
+    private final RSAKeyRecord rsaKeyRecord;
+    private final JwtTokenUtils jwtTokenUtils;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Order(1)
+    @Bean
+    public SecurityFilterChain signInSecurityFilterChain(HttpSecurity httpSecurity)
+            throws Exception{
+
+        return httpSecurity
+                .securityMatcher(new AntPathRequestMatcher("/sign-in/**"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .userDetailsService(userDetailsConfig)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex->{
+                    ex.authenticationEntryPoint(((request, response, authException) ->
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage())));
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+    }
+
+    @Order(2)
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+        throws Exception{
+        return httpSecurity
+                .securityMatcher(new AntPathRequestMatcher("/api/**"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .userDetailsService(userDetailsConfig)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(rsaKeyRecord.rsaPublicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(rsaKeyRecord.rsaPublicKey()).privateKey(rsaKeyRecord.rsaPrivateKey()).build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return  new NimbusJwtEncoder(jwkSource);
+    }
+
 
 }
